656b4deab4105fc6b9b19dea33a57add1300a457
Wisdom Ogwu <iammadab@gmail.com>
2025-03-06 12:50:33 +0100

    add stride computation on tensor creation

--- src/tensor.rs
+++ src/tensor.rs
@@ -4,6 +4,7 @@ type Shape = Vec<usize>;
 struct Tensor<T> {
     data: Vec<T>,
     shape: Shape,
+    strides: Vec<usize>,
 }
 
 impl<T: Default + Clone> Tensor<T> {
@@ -16,7 +17,29 @@ impl<T: Default + Clone> Tensor<T> {
     fn empty_from_shape(shape: Shape) -> Self {
         Self {
             data: vec![T::default(); shape.iter().product()],
+            strides: compute_strides(&shape),
             shape,
         }
+    }
+}
+
+fn compute_strides(shape: &Shape) -> Vec<usize> {
+    let mut strides = vec![1; shape.len()];
+    for i in (0..shape.len() - 1).rev() {
+        strides[i] = strides[i + 1] * shape[i + 1];
+    }
+    strides
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tensor::Tensor;
+
+    #[test]
+    fn test_tensor_stride_construction() {
+        assert_eq!(
+            Tensor::<u32>::empty_from_shape(vec![2, 3, 2, 4]).strides,
+            vec![24, 8, 4, 1]
+        );
     }
 }
