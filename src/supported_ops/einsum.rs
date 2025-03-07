8c6ca97202a6884ac94b7d3e8fee266583588eb1
Wisdom Ogwu <iammadab@gmail.com>
2025-03-07 16:23:27 +0100

    split einsum instruction parsing into its own function

--- src/supported_ops/einsum.rs
+++ src/supported_ops/einsum.rs
@@ -4,6 +4,93 @@ use crate::tensor::tensor::Tensor;
 use std::collections::{BTreeSet, HashMap};
 
 // TODO: add documentation
+fn parse_einsum_instruction(
+    instruction: &str,
+    inputs: &[Tensor<usize>],
+) -> (Shape, Vec<FixedShapeGenerator>) {
+    // partition instruction to get input instruction and output instruction
+    let [input_insn, output_insn]: [&str; 2] = instruction
+        .split("->")
+        .take(2)
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    // get the dimension variable string for each input
+    let input_insn = input_insn.split(",").collect::<Vec<_>>();
+
+    // for each dimension variable figure out the concrete shape
+    let mut symbol_dimensions: HashMap<char, usize> = HashMap::new();
+    for (inst, input_tensor) in input_insn.iter().zip(inputs.iter()) {
+        for (char, dim) in inst.chars().zip(input_tensor.shape.dims.iter()) {
+            symbol_dimensions.insert(char, *dim);
+        }
+    }
+
+    // free variables represent variables in the output
+    let free_variables: BTreeSet<char> = output_insn.chars().collect();
+    let free_variable_positions: HashMap<char, usize> = free_variables
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, i))
+        .collect();
+
+    // TODO: handle empty output section here
+    // generate output shape and empty output tensor
+    let mut output_shape = vec![0; output_insn.len()];
+    for (i, char) in free_variables.iter().enumerate() {
+        output_shape[i] = symbol_dimensions[&char];
+    }
+    let output_shape = Shape::new(output_shape);
+
+    // create input to free variable mappings for all inputs
+    let input_iter_generators = input_insn
+        .iter()
+        .zip(inputs.iter())
+        .map(|(input_str, input_tensor)| {
+            FixedShapeGenerator::from(
+                input_str,
+                &free_variable_positions,
+                input_tensor.shape.clone(),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    (output_shape, input_iter_generators)
+}
+
+// TODO: add documentation
+fn einsum(insn: &str, inputs: &[Tensor<usize>]) -> Tensor<usize> {
+    let (output_shape, input_iter_generators) = parse_einsum_instruction(insn, inputs);
+    let mut output_tensor = Tensor::<usize>::new(None, output_shape);
+
+    // next we fill in each value for the output
+    for output_index in output_tensor.shape.index_iter(None) {
+        let input_iters = IndexZip::new(
+            input_iter_generators
+                .iter()
+                .map(|gen| gen.get_iter(&output_index))
+                .collect(),
+        );
+
+        *output_tensor.get_mut(&output_index) = input_iters
+            .map(|indices| {
+                indices
+                    .iter()
+                    .zip(inputs.iter())
+                    .map(|(index, tensor)| tensor.get(index))
+                    .product::<usize>()
+            })
+            .sum::<usize>()
+    }
+
+    // next we need to evaluate each input tensor at the production of their input indices
+    // once we do this we mul the result
+    output_tensor
+}
+
+/// Used to hold free variables to input variable mapping
+/// Can produce a shape indices iterator with fixed values based on mapping
 struct FixedShapeGenerator {
     // Vec<(source, target)>
     mapping: Vec<(usize, usize)>,
@@ -22,100 +109,13 @@ impl FixedShapeGenerator {
     }
 
     fn get_iter(&self, source_values: &[usize]) -> ShapeIndices {
-        let fix_insn = self.mapping.iter().map(|(source, target)| (*target, source_values[*source])).collect();
+        let fix_insn = self
+            .mapping
+            .iter()
+            .map(|(source, target)| (*target, source_values[*source]))
+            .collect();
         self.shape.index_iter(Some(fix_insn))
     }
-}
-
-// TODO: add documentation
-fn einsum(insn: &str, inputs: &[Tensor<usize>]) -> Tensor<usize> {
-    // assumes the instruction string is valid
-    // TODO: deal with unwrap
-    let [input_insn, output_insn]: [&str; 2] = insn
-        .split("->")
-        .take(2)
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
-
-    let input_insn = input_insn.split(",").collect::<Vec<_>>();
-
-    // now that I have the input instruction and the output instruction what is next??
-    // I need to do a dimension analysis
-    let mut symbol_dimensions: HashMap<char, usize> = HashMap::new();
-    for (inst, input_tensor) in input_insn.iter().zip(inputs.iter()) {
-        for (char, dim) in inst.chars().zip(input_tensor.shape.dims.iter()) {
-            symbol_dimensions.insert(char, *dim);
-        }
-    }
-
-    // given the symbol dimension, we can get the shape of all inputs and all outputs
-    // but what do we do with this shape??
-    // the goal is to create an empty tensor with the output shape
-    // then go through each index in that tensor
-    // but we want to fix values in the input, based on the output shape change
-    // how can we achieve this??
-    // let us call free variables the variables that appear in the output
-    // those variables are supposed to be fixed in the input
-    // everytime we get an output index, we should link the values with the concrete index value
-    // that can be done implicitly
-    // next we should generate fixed iterators for the input shapes
-    //
-
-    let free_variables: BTreeSet<char> = output_insn.chars().collect();
-    let free_variable_positions: HashMap<char, usize> = free_variables
-        .iter()
-        .enumerate()
-        .map(|(i, v)| (*v, i))
-        .collect();
-
-    // generate output shape
-    let mut output_shape = vec![0; output_insn.len()];
-    for (i, char) in free_variables.iter().enumerate() {
-        output_shape[i] = symbol_dimensions[&char];
-    }
-    let output_shape = Shape::new(output_shape);
-
-    let mut output_tensor = Tensor::<usize>::new(None, output_shape.clone());
-
-    // output shape is the same as free variables
-    // I'd like a map from free variable to the concrete output
-
-    // next we fill in each value for the output
-    for output_index in output_shape.index_iter(None) {
-        let mut fixed_input_shapes = vec![];
-        for inst in input_insn.iter() {
-            let mut fix_insn = vec![];
-            for (i, char) in inst.chars().enumerate() {
-                if let Some(pos) = free_variable_positions.get(&char) {
-                    fix_insn.push((i, output_index[*pos]))
-                }
-            }
-            fixed_input_shapes.push(fix_insn);
-        }
-
-        let mut input_iters = IndexZip::new(
-            fixed_input_shapes
-                .into_iter()
-                .zip(inputs.iter())
-                .map(|(fix_insn, tensor)| tensor.shape.index_iter(Some(fix_insn)))
-                .collect::<Vec<_>>(),
-        );
-
-        *output_tensor.get_mut(&output_index) = input_iters
-            .map(|indices| {
-                indices
-                    .iter()
-                    .zip(inputs.iter())
-                    .map(|(index, tensor)| tensor.get(index))
-                    .product::<usize>()
-            })
-            .sum::<usize>()
-    }
-
-    // next we need to evaluate each input tensor at the production of their input indices
-    // once we do this we mul the result
-    output_tensor
 }
 
 struct IndexZip {
@@ -166,7 +166,8 @@ mod tests {
         // assume we have some input string ijk
         // and we have some free variable j that comes from index 0 of source
         let free_variables: HashMap<char, usize> = vec![('j', 0)].into_iter().collect();
-        let shape_iter_generator = FixedShapeGenerator::from("ijk", &free_variables, Shape::new(vec![2, 5, 1]));
+        let shape_iter_generator =
+            FixedShapeGenerator::from("ijk", &free_variables, Shape::new(vec![2, 5, 1]));
         assert_eq!(shape_iter_generator.mapping, &[(0, 1)]);
         // this should create a generator that fixes j to 3
         let iter = shape_iter_generator.get_iter(&[3]);
