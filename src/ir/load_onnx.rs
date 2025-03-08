ce33bd3e92b1c2c47e5fbd65f3b7805caf718329
Wisdom Ogwu <iammadab@gmail.com>
2025-03-08 15:46:07 +0100

    implement source parsing

--- src/ir/load_onnx.rs
+++ src/ir/load_onnx.rs
@@ -1,8 +1,12 @@
+use std::fmt::Debug;
 use std::path::PathBuf;
 use tract_core::internal::tract_itertools::Itertools;
 
 use crate::ir::intermediate_representation::IR;
+use crate::ir::ops::tensor_view::{TensorViewOp, ViewType};
 use crate::ir::ops::Ops;
+use crate::ir::TractOps;
+use crate::tensor::shape::Shape;
 use tract_onnx::prelude::*;
 
 pub(crate) fn load_onnx(path: PathBuf) -> Graph<TypedFact, Box<dyn TypedOp>> {
@@ -35,17 +39,36 @@ pub(crate) fn model_graph_to_ir(model_graph: &Graph<TypedFact, Box<dyn TypedOp>>
 
     for node in &model_graph.nodes {
         let op = match node.op.name().as_ref() {
-            "Source" => parse_source(node, &mut input_count),
+            "Source" => parse_source(node.into(), &mut input_count),
             unknown_op => panic!("unsupported node: {}", unknown_op),
         };
+        dbg!(&op);
         ops.push(op);
     }
 
     IR::new(input_count, constants, output_ids, ops)
 }
 
-fn parse_source<F: Fact, O>(node: &Node<F, O>, input_index: &mut usize) -> Ops {
-    todo!()
+fn parse_source<F: Fact, O: Debug>(node: &Node<F, O>, input_index: &mut usize) -> Ops {
+    assert_eq!(node.outputs.len(), 1);
+    let shape_data = &node.outputs[0]
+        .fact
+        .to_typed_fact()
+        .expect("should be typed model")
+        .shape;
+    let shape_dims = shape_data
+        .as_concrete()
+        .expect("failed to get concrete shape");
+    let shape = Shape::new(shape_dims.to_vec());
+    let volume = shape.volume();
+    let op = Ops::TensorView(TensorViewOp {
+        id: node.id,
+        tensor_type: ViewType::Input,
+        start_index: *input_index,
+        shape,
+    });
+    *input_index += volume;
+    op
 }
 
 #[cfg(test)]
